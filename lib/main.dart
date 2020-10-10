1dcff4ecfcf99df5095375d5f86da84240e37b47
Puzzleleaf <cmtyx2@naver.com>
2020-10-10 22:22:59 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Kakaobank',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           color: Colors.transparent,
